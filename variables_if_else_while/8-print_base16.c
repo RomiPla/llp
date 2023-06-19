a62d8ab4442f70cedefd76b65da64685f3c959c2
root <plaromina1997@gmail.com>
2023-06-19 05:23:21 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Numbers
+*Return: is 0
+*/
+
+	int main(void)
+	{
+		char numbers;
+
+		for (numbers = '0'; numbers <= '9' ; numbers++)
+{
+		putchar(numbers);
+		}
+	{
+		char alphabet = 'a'
+	while (alphabet <= 'f')
+	}
+	putchar('\n');
+	return (0);
+}

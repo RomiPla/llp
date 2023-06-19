b2f622541c668583619a49bc3f4fe12d02e62e7d
root <plaromina1997@gmail.com>
2023-06-19 10:01:15 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
  * Return: Always 0.
  */
 void print_alphabet(void)
-{	
+{
 	char alphabet;
-	
+
 	alphabet = 'a';
 	while (alphabet <= 'z')
-	{ 
+	{
 		_putchar(alphabet);
 		alphabet++;
 	}

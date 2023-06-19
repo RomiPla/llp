33dce5808594855417bfc1c72894814ec3885216
root <plaromina1997@gmail.com>
2023-06-18 18:40:07 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Numbers
+*Return: is 0
+*/
+
+	int main(void)
+	{
+		char numbers;
+
+		for (numbers = '0'; numbers <= '9' ; numbers++)
+{
+		putchar(numbers);
+		}
+	putchar('\n');
+	return (0);
+}

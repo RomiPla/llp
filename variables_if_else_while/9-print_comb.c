b8130678dae4e3e72a7690e941cc0ef98beb1062
root <plaromina1997@gmail.com>
2023-06-19 06:55:00 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -4,16 +4,19 @@
 *Return: is 0
 */
 
-	int main(void)
-	{
+int main(void)
+{
 		int numbers;
 
-		for (numbers = '0'; numbers < '9' ; numbers++)
-{
+		for (numbers = '0'; numbers <= '9' ; numbers++)
+		{
 		putchar(numbers);
-		putchar(',');
-		putchar(' ');
+			if (numbers < '9')
+			{
+			putchar(',');
+			putchar(' ');
+			}
 		}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }

4fe6a71f5cf9b2692f1a3f691710de30a1a22dd1
root <plaromina1997@gmail.com>
2023-06-19 05:28:10 -0700

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,13 +8,13 @@
 	{
 		char numbers;
 
-		for (numbers = '0'; numbers <= '9' ; numbers++)
+		for (numbers = '0'; numbers <= '9' ; numbers++);
 {
 		putchar(numbers);
 		}
 	{
-		char alphabet = 'a'
-	while (alphabet <= 'f')
+		char alphabet = 'a';
+	while (alphabet <= 'f');
 	}
 	putchar('\n');
 	return (0);

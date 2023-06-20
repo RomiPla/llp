e192d8cf8275d28139e575d501286bb2d5618cf9
root <plaromina1997@gmail.com>
2023-06-20 08:53:55 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,30 +12,31 @@ void times_table(void)
 	for (n = 0; n <= 9; n++)
 	{
 		for (m = 0; m <= 9; m++)
-
+		{
 			o = n * m;
 
-		if (o > 9)
-		{
-			p = o % 10;
-			q = (o - p) / 10;
-
-			_putchar(44);
-			_putchar(32);
-			_putchar(q + '0');
-			_putchar(p + '0');
-		}
-		else
-		{
-			if (m != 0)
+			if (o > 9)
 			{
+				p = o % 10;
+				q = (o - p) / 10;
+
 				_putchar(44);
 				_putchar(32);
-				_putchar(32);
+				_putchar(q + '0');
+				_putchar(p + '0');
 			}
+			else
+			{
+				if (m != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
 
-			_putchar(o + '0');
+				_putchar(o + '0');
+			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

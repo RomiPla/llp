fc6fbd1a3f5ed32e9b9d1b519955edb4f2e27141
root <plaromina1997@gmail.com>
2023-06-20 09:59:46 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *add - It adds one value to another
+  *@a:is a number
+  *@b: is another number
+  *Return: On Success (0)
+  */
+int add(int a, int b)
+{
+	int n = a + b;
+
+		return (n);
+}

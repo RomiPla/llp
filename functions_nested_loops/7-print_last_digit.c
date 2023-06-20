cbcd1c970efc600ed8b54883ca222cbaff560261
root <plaromina1997@gmail.com>
2023-06-20 06:15:38 -0700

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,13 @@
 /**
   *print_last_digit - last number
   *@n: letter in question
-  *Return Always seccess
+  *Return: Always seccess
   */
 
 int print_last_digit(int n)
 {
 	int ld = n % 10;
-	if (ld < 0)
+		if (ld < 0)
 		ld *= -1;
 	_putchar(ld + '0');
 	return (ld);

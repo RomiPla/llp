4353717255f5be867a0377fb1fa79449a3ea87b9
root <plaromina1997@gmail.com>
2023-06-22 05:39:01 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Updates the value it points to 98
+ *@n: A variable that changes value to 98
+ */
+void reset_to_98(int *n)
+{
+	 *n = 98;
+}

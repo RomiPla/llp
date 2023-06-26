63083b8ae64e535e1751cc43120614d9488ac6c9
root <plaromina1997@gmail.com>
2023-06-26 10:02:10 -0700

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,12 +10,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
-	
-	while(dest[i] != '\0')
+
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	for (j=0; j<n && src[j] != '\0';)
+	for (j = 0; j < n && src[j] != '\0';)
 	{
 		dest[i] = src[j];
 		i++;

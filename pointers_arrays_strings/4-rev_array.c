9cfde89ede2fbe73cde1a6f0846deabbb28f97cc
root <plaromina1997@gmail.com>
2023-06-27 06:15:50 -0700

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  *reverse_array - Reverses an array
+  *
+  *@n: in the number of elements
+  *
+  *@a: the array to be reversed
+  *
+  *return void
+  */
+
+void reverse_array(int *a, int n)
+{
+	int r;
+	int o;
+	int temp;
+
+
+	for (r = 0, r = n - 1; r < o; r++, o--)
+	{
+		temp = a[r];
+		a[r] = a[o];
+		a[o] = temp;
+	}
+}

a09c5423f86a393942f28c0362d4fdca36d3cc0a
root <plaromina1997@gmail.com>
2023-07-06 06:38:03 -0700

    2-str_concat.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,12 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
-  * _strdup - Duplicate a string
-  * @str: the string to duplicate
-  *
-  * Return: the string duplicated
-  */
 
 char
 *_strdup(char *str)
---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *str_concat - concatenate strings using malloc
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer of concat string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *string;
+	int a, b, x
+
+	if (s1 == '\0')
+
+		s1 = ""
+
+	if (s2 == '\0')
+		s2 = ""
+
+for (x = 0; s1[x] != '\0'; x++)
+		a++
+
+for (x = 0; s2[x] != '\0'; x++)
+		b++
+
+*string = malloc(size of (char)*b + a + 1)
+
+if (!*string)
+	return (NULL)
+
+for (x = 0; x != a; x++)
+	*string[x] = s1[x]
+
+for (x = 0; x != b; a++, x++)
+
+	*string[a] = s2[x]
+		return (*string)

21f4c24cb50792282041ec3a3407d4cd1e34bb67
root <plaromina1997@gmail.com>
2023-07-12 05:43:15 -0700

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+void 
+array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int count = 0;
+
+	if (array != NULL && action != NULL)
+	{
+		for (; count < size; count++)
+		{
+			action(array[count]);
+		}
+	}
+}

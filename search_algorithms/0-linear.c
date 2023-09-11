c4b28a3ad5ed70aab1705c3a406f3b9d82349506
root <plaromina1997@gmail.com>
2023-09-11 06:23:22 -0700

    0-linear.c

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+  * linear_search - Nombre de la funcion
+  * array: fruriuhuh
+  * size: uifhufdhsdhfd
+  * value: -1 o 1
+  *
+  **/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t R = 0;
+
+	if (array == NULL)
+	{
+		return -1;
+	}
+
+
+	for (; R < size; R++)
+	{
+		printf ("Value checked array[%lu] = [%d]\n", R, array[R]);
+		if (array[R] == value)
+		{
+			return 1;
+		}
+	}
+	return -1;
+}
